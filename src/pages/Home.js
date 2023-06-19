58edb75c1f568f46d40810cb027679bd804483d1
Yehia Helal <mangetsuhozuki287@yahoo.com>
2023-06-19 16:04:54 +0300

    55

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,7 @@ const Home = () => {
   const { user, dispatchUser } = useAuthContext();
   const [slidingImagesOffer, setSlidingImagesOffer] = useState(1);
   const [slidingThirtyOfferItems, setSlidingThirtyOfferItems] = useState(1);
+  const [showAlertNotifcation, setshowAlertNotifcation] = useState(true);
 
   // const firstTen = allItems.slice(0, 10);
 
@@ -36,6 +37,16 @@ const Home = () => {
     };
 
     fetchTours();
+  }, [dispatcho]);
+
+  useEffect(() => {
+    if (showAlertNotifcation) {
+      alert(
+        "Hello! ,Please wait 30 seconds for the Backend/Database server to start working again (because it's a free hosting after 15min of inactivity it stops working)"
+      );
+
+      setshowAlertNotifcation(false);
+    }
   }, [dispatcho]);
 
   useEffect(() => {
